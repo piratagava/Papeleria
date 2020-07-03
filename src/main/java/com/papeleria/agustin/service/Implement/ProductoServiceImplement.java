9afdcc3806e5aab48e1d5504081e79abe1a1bd3c
Ghost <hernangava1524@gmail.com>
2020-07-02 19:26:23 -0500

    Implementacion de Metodo listar y eliminar

--- src/main/java/com/papeleria/agustin/service/Implement/ProductoServiceImplement.java
+++ src/main/java/com/papeleria/agustin/service/Implement/ProductoServiceImplement.java
@@ -1,9 +1,12 @@
 package com.papeleria.agustin.service.Implement;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.papeleria.agustin.Exceptions.ProductoExceptions;
 import com.papeleria.agustin.model.Producto;
 import com.papeleria.agustin.repository.ProductoRepository;
 import com.papeleria.agustin.service.ProductoService;
@@ -17,14 +20,36 @@ public class ProductoServiceImplement implements ProductoService {
 	private ProductoRepository productoRepository;
 
 	@Override
-	public void guardar(Producto producto) {
-		if(producto.getNombre_producto()==null || producto.getPrecio_venta()<0 || producto.getStock_minimo()<0 || producto.getStock_actual()<0 
-				|| producto.getCod_barra()==null || producto.getDescripcion()==null){
-			log.error("Existe Valores Vacios");
-		}else {
-			productoRepository.save(producto);	
+	public void guardar(Producto producto) throws ProductoExceptions {
+		StringBuilder mensaje = new StringBuilder();
+		try {
+			if (producto.getNombre_producto() == null || producto.getPrecio_venta() <= 0
+					|| producto.getStock_minimo() <= 0 || producto.getStock_actual() <= 0
+					|| producto.getCod_barra() == null || producto.getDescripcion() == null) {
+				log.warn("Algunos datos de producto vienen vacios o con 0 para su registro en BD");
+				// Genera el error dentro de la ejecucion
+				throw new ProductoExceptions(
+						"EXISTEN ATRIBUTOS VACIOS NO SE PUEDE GUARDAR REGISTRO EN LA BD-> PRODUCTO");
+			} else {
+				productoRepository.save(producto);
+			}
+		} catch (ProductoExceptions e) {
+			mensaje.append("NO SE PUDO GUARDAR EL PRODUCTO ERROR INTERNO").append(e.getMessage());
+			log.error("ERROR NO SE PUDO REGISTRAR NADA");
+			throw new ProductoExceptions(e.getMessage());
 		}
-		
+
 	}
+
+	@Override
+	public List<Producto> listarTodosProductos() {
+		return productoRepository.findAll();
+	}
+
+	@Override
+	public void eliminar(Producto producto) throws ProductoExceptions {
+		productoRepository.delete(producto);
+	}
+
 
 }
