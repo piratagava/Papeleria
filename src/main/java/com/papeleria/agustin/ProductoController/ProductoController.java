b423cd15841acc97b0d5aaea0597faa6fa653fad
Ghost <hernangava1524@gmail.com>
2020-07-02 19:24:20 -0500

    Validando Backend Producto no vacio

--- src/main/java/com/papeleria/agustin/ProductoController/ProductoController.java
+++ src/main/java/com/papeleria/agustin/ProductoController/ProductoController.java
@@ -1,17 +1,24 @@
 package com.papeleria.agustin.ProductoController;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.papeleria.agustin.Exceptions.ProductoExceptions;
 import com.papeleria.agustin.model.Producto;
 import com.papeleria.agustin.service.ProductoService;
 
 @Controller
 public class ProductoController {
+	
 	
 	@Autowired 
 	private ProductoService productoService;
@@ -26,8 +33,31 @@ public class ProductoController {
 		try {
 			productoService.guardar(producto);
 			return true;
+		} catch (ProductoExceptions e) {
+			System.out.print("ERROR EN GUARDAR POR "+ e.toString());
+			System.out.print("MENSAJE DE ERROR INTERNO NO SE GUARDO EL PRODUCTO "+ e.getMessage());
+			return false;
+		}	
+	}
+	
+	@RequestMapping(value="/listaProducto", method = RequestMethod.GET)
+	public @ResponseBody List<Producto> todosLosProductos() {
+		try {
+			List<Producto> listarTodos = productoService.listarTodosProductos();
+			return listarTodos;
 		} catch (Exception e) {
-			System.out.print("ERROR EN GUARDAR POR "+ e);
+			System.out.print("ERROR EN LISTAR POR "+ e.toString());
+			System.out.print("MENSAJE DE ERROR INTERNO NO SE PUDO LISTAR LOS PRODUCTOS "+ e.getMessage());
+			return null;
+		}		
+	}
+	
+	@RequestMapping(value="/eliminarProducto", method = RequestMethod.POST)
+	public @ResponseBody Boolean eliminarProducto(@RequestBody Producto producto) {
+		try {
+			 productoService.eliminar(producto);
+			 return true;
+		} catch (ProductoExceptions e) {
 			return false;
 		}
 		
