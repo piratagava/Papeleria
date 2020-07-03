07a3a4373fc5f89b5a1c257192214dd7384ad8b3
Ghost <hernangava1524@gmail.com>
2020-07-02 19:25:30 -0500

    Creacion de metodo Listar y eliminar

--- src/main/java/com/papeleria/agustin/service/ProductoService.java
+++ src/main/java/com/papeleria/agustin/service/ProductoService.java
@@ -1,8 +1,15 @@
 package com.papeleria.agustin.service;
 
+import java.util.List;
+
+import com.papeleria.agustin.Exceptions.ProductoExceptions;
 import com.papeleria.agustin.model.Producto;
 
 public interface ProductoService {
+			
+	public void guardar(Producto producto) throws ProductoExceptions;
 	
-	public void guardar(Producto producto);
+	public List<Producto> listarTodosProductos();
+	
+	public void eliminar(Producto producto) throws ProductoExceptions;
 }
